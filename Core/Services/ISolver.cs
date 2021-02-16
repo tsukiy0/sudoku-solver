5ff10432b8148318aaad1f20b4c739116467f7c4
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-16 20:32:49 +1100

    test: add tests harness for ISolver

--- Core/Services/ISolver.cs
+++ Core/Services/ISolver.cs
@@ -1,9 +1,10 @@
+using System.Collections.Generic;
 using Core.Models;
 
 namespace Core.Services
 {
     public interface ISolver
     {
-        IBoard Solve(IBoard board, IBoardRule[] rules);
+        IBoard Solve(IBoard board, IEnumerable<IBoardRule> rules);
     }
 }
