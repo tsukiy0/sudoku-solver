c04de878e8d60d653624c54d71c993f0030136f3
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-16 20:08:22 +1100

    feat: add SquareRuleTests

--- Core/Services/SquareRule.cs
+++ Core/Services/SquareRule.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using Core.Models;
 
 namespace Core.Services
@@ -6,7 +8,18 @@ namespace Core.Services
     {
         public bool Test(IBoard board, Point point, int value)
         {
-            throw new System.NotImplementedException();
+            var x0 = (int)Math.Floor((decimal)point.X / 3) * 3;
+            var y0 = (int)Math.Floor((decimal)point.Y / 3) * 3;
+
+            var square = Enumerable.Range(0, 3).SelectMany(x =>
+            {
+                return Enumerable.Range(0, 3).Select(y =>
+                {
+                    return board.Get(new Point(x0 + x, y0 + y));
+                });
+            });
+
+            return !square.Any(_ => _ == value);
         }
-}
+    }
 }
